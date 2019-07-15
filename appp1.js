96b2c5f9e1f56eb3274318c13e1130e92229e109
Marija Redzepovic <marijadarkova@gmail.com>
2019-07-15 23:34:29 +0200

    Update appp1.js

--- appp1.js
+++ appp1.js
@@ -7,7 +7,7 @@ var car = {
     }
 }
 
-var mike = Object.create(car);
+var mike = Object.create(car);//first we create empty object which is prototype at the car object, and this object have access of all metods and properties in the car object
 mike.nameCar = 'golf';
 mike.color = 'red';
 mike.driver = 'Mike';
@@ -16,6 +16,7 @@ var dona = Object.create(car);
 dona.nameCar = 'passat';
 dona.color = 'blue';
 dona.driver = 'Dona';
-
+//a both object now share same drive function
+//both of this object point at the car object as the their prototype
 console.log(mike.drive());
 console.log(dona.drive());
